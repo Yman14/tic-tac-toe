b2c5208696efb94c3b155bf104a192e9fe1ed50b
Yman <ymanneil07@gmail.com>
2026-04-08 18:21:54 +0800

    create gameboard for the game

--- scripts.js
+++ scripts.js
@@ -1,2 +1,27 @@
 const greet = document.querySelector("h1");
 greet.textContent = "Tic Tac Toe Game";
+console.log("console is running.");
+
+function Gameboard() {
+    const boardSize = 3;
+    let board = [];
+
+    const createBoard = () => {
+        for(let i = 0; i < boardSize; i++)
+        {
+            board[i] = [];
+            for(let j = 0; j < boardSize; j++)
+            {
+                board[i][j] = " ";
+            }
+        }
+    };
+
+   const getBoard = () => board;
+
+   return {createBoard, getBoard};
+};
+
+const game = Gameboard();
+game.createBoard();
+console.log(game.getBoard());
