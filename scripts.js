11e0951dd6e5eab00e76787addf42414410485b1
Yman <ymanneil07@gmail.com>
2026-04-09 13:24:31 +0800

    create simple construction for player and gameplay

--- scripts.js
+++ scripts.js
@@ -22,6 +22,40 @@ function Gameboard() {
    return {createBoard, getBoard};
 };
 
+function CreatePlayer(name, marker) {
+    this.name = name;
+    this.marker = marker;
+    let score = 0;
+
+    const win = () => { score++; };
+    const getScore = () => score;
+    return{name, marker, win, getScore};
+};
+
+function Gameplay () {
+    const board = Gameboard();
+    board.createBoard();
+    const p1 = CreatePlayer("PLAYER", "X");
+
+    const playRound = (i, j) => {
+        board.getBoard()[i][j] = p1.marker;
+    };
+
+    return {board, p1, playRound};
+};
+
+
+//Lab
 const game = Gameboard();
 game.createBoard();
 console.log(game.getBoard());
+
+const player1 = CreatePlayer("Lee", "X");
+console.log(player1);
+console.log(player1.getScore());
+player1.win();
+console.log(player1.getScore());
+console.log(player1);
+
+const demo = Gameplay();
+console.log(demo);
