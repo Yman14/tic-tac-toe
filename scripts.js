c5ddc9712bc613f75655c5746cff243d04faaca8
Yman <ymanneil07@gmail.com>
2026-04-09 18:44:27 +0800

    initial gameplay

--- scripts.js
+++ scripts.js
@@ -1,5 +1,3 @@
-const greet = document.querySelector("h1");
-greet.textContent = "Tic Tac Toe Game";
 console.log("console is running.");
 
 function Gameboard() {
@@ -36,26 +34,108 @@ function Gameplay () {
     const board = Gameboard();
     board.createBoard();
     const p1 = CreatePlayer("PLAYER", "X");
+    const p2 = CreatePlayer("COMPUTER", "O");
+    let player1Move = true;
 
-    const playRound = (i, j) => {
-        board.getBoard()[i][j] = p1.marker;
+    const playerController = (i, j, p) => {
+        board.getBoard()[i][j] = p.marker;
     };
 
-    return {board, p1, playRound};
+    const playerMove = (i, j) => {
+        playerController(i, j, p1);
+        player1Move = false;
+    };
+
+    const computerMove = (i, j) => {
+        playerController(i, j, p2);
+        player1Move = true;
+    };
+
+    const getMove = () => {
+        const input = getInput();
+        if(player1Move){
+            playerMove(parseInt(input.charAt(0)), parseInt(input.charAt(1)));
+        }else{
+            computerMove(parseInt(input.charAt(0)), parseInt(input.charAt(1)));
+        }
+    };
+
+    const getInput = () => {
+        return prompt("Move: ");
+    };
+
+    const displayBoard = ()=> board.getBoard();
+    const displayBoard1 = ()=> {
+        for(let i = 0; i <= board.boardSize; i++)
+        {
+            for(let j = 0; j <= board.boardSize; j++)
+            {
+                console.log(board.getBoard()[i][j]);
+            }
+        }
+    };
+
+    return {board, p1, p2, playerController, playerMove, computerMove, getInput, getMove, displayBoard, displayBoard1};
 };
+
+(function StartGame(){
+    console.log("Game Start!");
+    const game = Gameplay();
+    console.log(game.displayBoard());
+    console.log(game.displayBoard1());
+    console.log(game.board.getBoard()[1][0]);
+    console.log(game.board.getBoard()[1][0]);
+    console.log(game.board.getBoard()[1][0]);
+    for(let i = 0; i <= game.board.boardSize; i++)
+    {
+        for(let j = 0; j <= game.board.boardSize; j++)
+        {
+            console.log(game.board.getBoard()[i][j]);
+        }
+    }
+
+    // while(noOneWins()){
+    //     getMove();
+    // }
+    let n = 3;
+    while(n>0)
+    {
+        game.getMove();
+        n--;
+    }
+    console.log(game.board.getBoard()[1][0]);
+    console.log(game.board.getBoard()[1][1]);
+    console.log(game.board.getBoard()[1][2]);
+      console.log(game.board.boardSize);
+    for(let i = 0; i <= game.board.boardSize; i++)
+    {
+        for(let j = 0; j <= game.board.boardSize; j++)
+        {
+            console.log(game.board.getBoard()[i][j]);
+        }
+    }
+
+    // console.log("Player Move: ");
+    // input = game.getInput();
+    // game.playerMove(parseInt(input.charAt(0)), parseInt(input.charAt(1)));
+    // console.log(game.displayBoard());
+
+    // console.log("Computer Move: ");
+    // input = game.getInput();
+    // game.computerMove(parseInt(input.charAt(0)), parseInt(input.charAt(1)));
+    // console.log(game.displayBoard());
+
+
+})();
 
 
 //Lab
-const game = Gameboard();
-game.createBoard();
-console.log(game.getBoard());
-
-const player1 = CreatePlayer("Lee", "X");
-console.log(player1);
-console.log(player1.getScore());
-player1.win();
-console.log(player1.getScore());
-console.log(player1);
-
-const demo = Gameplay();
-console.log(demo);
+// const demo = Gameplay();
+// console.log(demo);
+// console.log(demo.getBoard());
+// demo.playerMove(1,1);
+// console.log(demo.getBoard());
+// demo.computerMove(1,2);
+// console.log(demo.getBoard());
+// demo.playerMove(2,0);
+// console.log(demo.getBoard());
